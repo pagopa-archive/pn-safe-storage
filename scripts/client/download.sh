25cafb7df85431196966ba174e2e0b4331b11c64
marco-vit-pagopa <marco.vit@pagopa.it>
2022-06-13 18:29:31 +0200

    Script di test per la condivisione di file tra utenti

--- scripts/client/download.sh
+++ scripts/client/download.sh
@@ -1,18 +1,121 @@
 #!/usr/bin/env bash
 
-objectKey=$1
 
-curl -v -X GET \
-  -H "x-pagopa-safestorage-cx-id: pn-delivery-002" \
-  https://07ivpqaag5.execute-api.eu-south-1.amazonaws.com/dev/safe-storage/v1/files/$objectKey \
+#!/usr/bin/env bash
+    
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+    
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+    
+usage() {
+      cat <<EOF
+    Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-f] -p param_value arg1 [arg2...]
+    
+    Script description here.
+    
+    Available options:
+    
+    -h, --help        Print this help and exit
+    -v, --verbose     Print script debug info
+    -a, --api         API Endpoint  (mandatory)
+    -k, --doc-key     Key of the document to download (mandatory)
+    -p, --stage       Stage (default: dev)
+    -c, --cx          (cx: default pn-delivery-001)
+EOF
+  exit
+}
+
+    
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+  
+
+setup_colors() {
+   if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+     NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+   else
+        NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+   fi
+}
+    
+msg() {
+  echo >&2 -e "${1-}"
+}
+    
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
+parse_params() {
+  # default values of variables set from params
+  doc_key=""
+  api_endpoint=''
+  cx='pn-delivery-001'
+  doc_type=''
+  file=''
+  stage='dev'
+
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose) set -x ;;
+    --no-color) NO_COLOR=1 ;;
+    -p | --stage) 
+      stage="${2-}"
+      shift
+      ;;
+    -k | --doc-key) 
+      doc_key="${2-}"
+      shift
+      ;;
+    -a | --api) 
+      api_endpoint="${2-}"
+      shift
+      ;;
+    -c | --cx) 
+      cx="${2-}"
+      shift
+      ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  args=("$@")
+
+  # check required params and arguments
+  [[ -z "${api_endpoint-}" ]] && die "Missing required parameter: api_endpoint"
+  [[ -z "${doc_key-}" ]] && die "Missing required parameter: doc-key"
+#  [[ ${#args[@]} -eq 0 ]] && die "Missing script arguments"
+
+  return 0
+}
+
+parse_params "$@"
+setup_colors
+
+curl -X GET \
+  -H "x-pagopa-safestorage-cx-id: ${cx}" \
+  https://${api_endpoint}/${stage}/safe-storage/v1/files/${doc_key} \
   \
   | tee > ${TMPDIR}/out.txt
 
-url=$( cat ${TMPDIR}/out.txt | jq -r ".download.uri")
+cat ${TMPDIR}/out.txt
+
+url=$( cat ${TMPDIR}/out.txt | jq -r ".download.url")
 key=$( cat ${TMPDIR}/out.txt | jq -r ".key")
+echo ""
+echo "PRESIGNED URL: $url" 
+echo "KEY: $key"
 
-
-curl -v -X GET \
+curl -X GET \
   -H "Content-TYpe: application/pdf" \
   --output ${key}.pdf \
   $url
---
+++ scripts/client/uploadAndDownload.sh
@@ -0,0 +1,31 @@
+# SCRIPT per testare la "condivisione di file tra più utenti"
+
+api_domain=14ej9p2krd.execute-api.eu-south-1.amazonaws.com
+#api_domain=ewlg4idc89.execute-api.eu-south-1.amazonaws.com
+
+
+./upload_file.sh -a $api_domain -f multa.pdf -t PN_NOTIFICATION_ATTACHMENTS -s SAVED -c pn-delivery | tee up_and_down.txt
+
+key=$( cat up_and_down.txt | grep 'Key:' | sed -e 's/Key: //')
+echo "UPLOAD WITH KEY: ${key}"
+
+sleep 5
+echo ""
+echo ""
+echo ""
+echo ""
+echo "===                DOWNLOAD WITH PN-DELIVERY                ==="
+echo "==============================================================="
+./download.sh -a $api_domain -k ${key} -c pn-delivery
+mv ${key}.pdf ${key}_delivery.pdf
+
+sleep 1
+echo ""
+echo ""
+echo ""
+echo ""
+echo "===              DOWNLOAD WITH PN-DELIVERY-PUSH             ==="
+echo "==============================================================="
+./download.sh -a $api_domain -k ${key} -c pn-delivery-push 
+mv ${key}.pdf ${key}_delivery_push.pdf
+
