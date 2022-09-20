4111fb71ff1138a5c16dfeffb2e7b39f3202f914
marco-vit-pagopa <marco.vit@pagopa.it>
2022-09-20 09:01:58 +0200

    Script per riprodurre problema di aggiornamento metadati

--- scripts/client/uploadAndMetadata.sh
+++ scripts/client/uploadAndMetadata.sh
@@ -1,20 +1,55 @@
 # SCRIPT per testare la "condivisione di file tra più utenti"
 
+# Ambiente sandbox
 #api_domain=14ej9p2krd.execute-api.eu-south-1.amazonaws.com
-api_domain=ewlg4idc89.execute-api.eu-south-1.amazonaws.com
-#api_domain=eo6pdvms24.execute-api.eu-south-1.amazonaws.com
-stage='dev'
+#stage='dev'
+
+# Ambiente SVIL
+# api_domain=ewlg4idc89.execute-api.eu-south-1.amazonaws.com
+# stage='dev'
+
+# Ambiente COLL
+api_domain=eo6pdvms24.execute-api.eu-south-1.amazonaws.com
+stage='coll'
 
 
-./upload_file.sh -a $api_domain -f multa.pdf -t PN_LEGAL_FACTS -s SAVED -c pn-delivery-push | tee up_and_down1.txt
-key1=$( cat up_and_down1.txt | grep 'Key:' | sed -e 's/Key: //')
-echo "UPLOAD WITH KEY: ${key1}"
-./upload_file.sh -a $api_domain -f multa.pdf -t PN_LEGAL_FACTS -s SAVED -c pn-delivery-push | tee up_and_down2.txt
-key2=$( cat up_and_down2.txt | grep 'Key:' | sed -e 's/Key: //')
-echo "UPLOAD WITH KEY: ${key2}"
-echo "### UPDATE METADATA on ${api_endpoint}"
-curl -fvs -H"x-pagopa-safestorage-cx-id: pn-delivery-push" \
--XPOST https://${api_domain}/${stage}/safe-storage/v1/files/{key1} -d"{\"status\": \"ATTACHED\",\"retentionUntil\": \"2022-09-17T14:59:28.028Z\"}"
+./upload_file.sh -a $api_domain -p $stage -f multa.pdf -t PN_NOTIFICATION_ATTACHMENTS -s PRELOADED -c pn-delivery \
+  | tee up_and_metadata.txt
+key1=$( cat up_and_metadata.txt | grep 'Key:' | sed -e 's/Key: //')
+echo ""
+echo "====> UPLOADED 1 WITH KEY: ${key1}"
 
-curl -fvs -H"x-pagopa-safestorage-cx-id: pn-delivery-push" \
--XPOST https://${api_domain}/${stage}/safe-storage/v1/files/{key2} -d"{\"status\": \"ATTACHED\",\"retentionUntil\": \"2022-09-17T14:59:28.028Z\"}"
+./upload_file.sh -a $api_domain -p $stage -f multa.pdf -t PN_NOTIFICATION_ATTACHMENTS -s PRELOADED -c pn-delivery \
+  | tee up_and_metadata.txt
+key2=$( cat up_and_metadata.txt | grep 'Key:' | sed -e 's/Key: //')
+echo ""
+echo "====> UPLOADED 2 WITH KEY: ${key2}"
+
+
+echo ""
+echo ""
+echo ""
+echo "########################################################"
+echo "###                 WAIT 10 SECONDS                  ###"
+echo "########################################################"
+sleep 10
+echo "====> GET FILE METADATA FOR KEY 1: ${key1} "
+./download.sh -a $api_domain -p $stage -k ${key1} -c pn-delivery-push 
+echo "====> GET FILE METADATA FOR KEY 2: ${key2} "
+./download.sh -a $api_domain -p $stage -k ${key2} -c pn-delivery-push 
+
+echo ""
+echo ""
+echo ""
+echo "====> TRY TO UPDATE METADATA FOR KEY 1: ${key1}"
+curl -v -H "Content-Type: application/json" -H"x-pagopa-safestorage-cx-id: pn-delivery" \
+    -XPOST https://${api_domain}/${stage}/safe-storage/v1/files/${key1} -d"{\"status\": \"ATTACHED\" }"
+
+echo ""
+echo ""
+echo ""
+echo "====> TRY TO UPDATE METADATA FOR KEY 2: ${key2}"
+curl -v -H "Content-Type: application/json" -H"x-pagopa-safestorage-cx-id: pn-delivery" \
+    -XPOST https://${api_domain}/${stage}/safe-storage/v1/files/${key2} -d"{\"status\": \"ATTACHED\" }"
+
+
